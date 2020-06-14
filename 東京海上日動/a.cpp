1ba5ef96a7b3415e67d898855addec57bfccd1fd
Koudai <2110koudai@gmail.com>
2020-06-14 09:30:27 +0900

    first commit

---
+++ 東京海上日動/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string a,res;
+
+    cin >> a;
+
+    res = a.substr(0,3);
+
+    cout << res << endl;
+}
---
+++ 東京海上日動/b.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
+{
+    ll a, v, b, w, t, apos, bpos;
+
+    cin >> a >> v >> b >> w >> t;
+
+    apos = a + v * t; //overflowになる可能背がある
+    bpos = b + w * t; //overflowになる可能背がある
+
+    if (apos >= bpos)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+//acできなかった
+/*解答
+
+int main()
+{
+    int a, b, v, w, t, d, s;
+
+    cin >> a >> v >> b >> w >> t;
+    d = abs(a - b); //絶対値
+    if (v <= w)
+    {
+        cout << "NO" << endl;
+        return 0;
+    }
+    s = v - w;
+    if (d <= (ll)s * t)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}*/
