b203f520143cc75b28d0131f0014756b155fa13c
kodai <2110koudai@gmail.com>
2021-02-14 08:34:22 +0900

    ABC177-A問題をAC

---
+++ ABC/ABC177/a.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+#define rep(i,n) for (int i=0;i<(n);i++)
+
+int main()
+{
+    int d,t,s;
+    cin >> d >> t >> s;
+    if(d<=s*t) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+	return 0;
+}
