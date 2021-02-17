cc8b33b411babcd8dfcd376544b305d535237b25
kodai <2110koudai@gmail.com>
2021-02-17 23:27:21 +0900

    ABC174-A問題をAC

---
+++ ABC/ABC174/a.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int,int>;
+#define rep(i,n) for (int i=0;i<(n);i++)
+
+int main()
+{
+    int x;
+    cin >> x;
+    if (x>=30) cout << "Yes" << endl;
+    else cout << "No" << endl;
+	return 0;
+}
