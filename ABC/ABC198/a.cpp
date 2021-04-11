54fbb8c0edcbedeef5799b58169f0cfcad168870
kodai <2110koudai@gmail.com>
2021-04-11 21:36:21 +0900

    ABC198-A,B問題をAC

---
+++ ABC/ABC198/a.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << n - 1 << endl;
+}
