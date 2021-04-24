06b460c4a0f377083e13455e9ef4b3e5bcca48a1
kodai <2110koudai@gmail.com>
2021-04-24 21:55:07 +0900

    ABC199-A問題をAC

---
+++ ABC/ABC199/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a * a + b * b < c * c)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+    return 0;
+}
